6c029ded5ef3f1a239b4525865ec09b6acd90598
vitoriapinheiro <vps3@cin.ufpe.br>
2022-05-18 16:35:12 -0300

    feature(levels): done it

--- nft-the-game/screens/LevelThree/index.tsx
+++ nft-the-game/screens/LevelThree/index.tsx
@@ -2,22 +2,67 @@ import React, { useEffect, useState } from 'react';
 import { RootTabScreenProps } from '../../types';
 import { Text } from '../../components/Themed';
 import {
+  FlatList,
+  StyleSheetProperties,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {
   View,
   Button,
+  LevelText,
+  ArrowContainer,
+  HeaderContainer,
+  TableContainer,
+  TableTitle,
+  IconContainer,
+  RowContainer,
 } from './styles';
 
 export default function LevelThreeScreen({ navigation }: RootTabScreenProps<'LevelThree'>) {
-  const [test, setTest] = useState();
+  const [explanation, setExplanation] = useState("Selecione a criptomoeda das estrelas");
+  const [nextLevel, setNextLevel] = useState(false);
+
+  const selectStatue = () => {
+    setExplanation("NFT não estão limitados a experiências virtuais como a maioria das pessoas imaginam");
+    NextLevel();
+  }
+
+  const NextLevel = () => {
+    setNextLevel(true);
+  }
+
   return (
-    <View>
-      <Text>
-        Level Three
-      </Text>
-      <Button onPress={() => {navigation.goBack()}}>
+    <TableContainer>
+    <HeaderContainer>
+      <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+        <ArrowContainer source={require('../../assets/images/arrow-left.png')} />
+      </TouchableWithoutFeedback>
+      <TableTitle>Nível Três</TableTitle>
+    </HeaderContainer>
+    <LevelText>{explanation}</LevelText>
+      <View>
+        <TouchableWithoutFeedback>
+          <RowContainer>
+            <IconContainer source={require('../../assets/images/stars.png')} /> 
+            <IconContainer source={require('../../assets/images/stars.png')} />
+            <IconContainer source={require('../../assets/images/stars.png')} />
+          </RowContainer>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback >
+          <IconContainer source={require('../../assets/images/rocket.png')} />
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => NextLevel()}>
+          <IconContainer source={require('../../assets/images/estrela.png')} />
+        </TouchableWithoutFeedback>
+      </View>
+      {nextLevel && (<Button onPress={() => {navigation.navigate("Quiz")}}>
+        <RowContainer>
         <Text>
-          Voltar
+          Parabéns, você está pronto para fazer nosso Quiz!
         </Text>
-      </Button>
-    </View>
+        <ArrowContainer source={require('../../assets/images/arrow-right.png')} />
+        </RowContainer>
+      </Button>)}
+  </TableContainer>
   );
 };
